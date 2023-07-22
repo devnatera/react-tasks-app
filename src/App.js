a69f63b53ea0f51148f313b2c64ac0f30e525676
devnatera <devnatera@gmail.com>
2023-07-22 11:42:06 -0500

    se crean los primeros componentes base

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './platzi.webp';
 import './App.css';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoCounter completed={3} total={5} />
+      <TodoSearch />
+      <TodoList>
+        <TodoItem />
+      </TodoList>
+      <CreateTodoButton />
     </div>
   );
 }
---
+++ src/TodoCounter.js
@@ -0,0 +1,9 @@
+function TodoCounter(props) {
+  return (
+    <h1>
+      Has completado {props.completed} de {props.total} TODOs
+    </h1>
+  );
+}
+
+export { TodoCounter };
