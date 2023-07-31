d8df2ba76412feac1d62beed6e154f385da759f0
devnatera <devnatera@gmail.com>
2023-07-31 14:13:14 -0500

    se implementa uso de localStorage

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 
-const defaultTodos = [
+/* const defaultTodos = [
   { text: 'Cortar cebolla', completed: true },
   { text: 'Tomar el curso de introducción a React', completed: false },
   { text: 'Llorar con la llorona', completed: false },
@@ -13,8 +13,20 @@ const defaultTodos = [
   { text: 'LALALALALALA', completed: false },
 ];
 
+localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
+localStorage.removeItem('TODOS_V1', defaultTodos); */
+
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+  let parsedTodos;
+  if (!localStorageTodos) {
+    localStorage.setItem('TODOS_V1', JSON.stringify([]));
+    parsedTodos = [];
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter(
@@ -30,13 +42,18 @@ function App() {
     }
   );
 
+  const saveTodos = (newTodos) => {
+    localStorage.setItem('TODOS_V1', JSON.stringify(newTodos));
+    setTodos(newTodos);
+  }
+
   const completeTodo = (text) => {
     const newTodos = [...todos];
     const todoIndex = newTodos.findIndex(
       (todo) => todo.text === text
     );
     newTodos[todoIndex].completed = true;
-    setTodos(newTodos);
+    saveTodos(newTodos);
   }
 
   const deleteTodo = (text) => {
@@ -45,7 +62,7 @@ function App() {
       (todo) => todo.text === text
     );
     newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
+    saveTodos(newTodos);
   }
 
   return (
--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -3,19 +3,19 @@ import './TodoCounter.css';
 
 function TodoCounter({ total, completed }) {
   const message = (total, completed) => {
-    if (total === completed) {
-      return (
-        <>
-          Felicitaciones! Has completado todos los TODOs
-        </>
-      );
-    } else {
-      return (
-        <>
-          Has completado <span>{completed}</span> de <span>{total}</span> TODOs
-        </>
-      );
+    let _message;
+    switch (total) {
+      case 0:
+        _message = <>Aun no has creado TODOs!</>;
+        break;
+      case (total === completed):
+        _message = <>Felicitaciones! Has completado todos los TODOs</>
+        break;
+      default:
+        _message = <>Has completado <span>{completed}</span> de <span>{total}</span> TODOs</>
+        break;
     }
+    return _message;
   }
 
   return (
